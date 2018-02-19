34cbac7b23a0f8764627c5773d48a15106073043
Stefano Ruschetta <stefano@MacBook-Pro-di-Stefano.local>
2018-02-19 13:44:41 +0100

    net server

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const net = require('net');
-
+/*
 var server = net.createServer(function(socket) {
 
    var connection_in = socket.remoteAddress + ":" + socket.remotePort;
@@ -8,3 +8,16 @@ var server = net.createServer(function(socket) {
 });
 
 server.listen(8989);
+*/
+
+const PORT = 5000;
+
+let server = net.createServer(onClientConnected);
+server.listen(PORT);
+
+function onClientConnected(socket) {
+    console.log(`New client: ${socket.remoteAddress}:${socket.remotePort}`);
+    socket.destroy();
+}
+
+console.log(`Server started on port: ${PORT}`);
