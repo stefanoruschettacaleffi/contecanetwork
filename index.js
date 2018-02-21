28f5f61523300e903fe2d45cb40bffefb449612c
Stefano Ruschetta <stefano@MacBook-Pro-di-Stefano.local>
2018-02-21 15:12:02 +0100

    e5

--- index.js
+++ index.js
@@ -1,15 +1,41 @@
 const net = require('net');
 const PORT = 8989;
 
-const server = net.createServer( function(socket) {
-
-    socket.on("data" , function(data) {
-       console.log( data.toString("hex"));
-    });
-});
+const server = net.createServer();
 
 
-server.listen(PORT);
+server.on('connection', handleConnection);
+
+server.listen(PORT , function(){
+    console.log("server listening to: %j", server.address());
+})
+
+function handleConnection(conn){
+
+
+    var remoteAddress = conn.remoteAddress + ":" + conn.remotePort;
+    console.log("New client connection from: " + remoteAddress);
+
+    conn.setEncoding("hex");
+
+    conn.on("data", onConnData);
+    conn.on("close", onConnClose);
+    conn.on("err", onConnErr);
+
+    function onConnData(data) {
+        console.log("Data received: " + data);
+    }
+
+    function onConnClose(){
+        console.log("Connection close");
+    }
+
+    function onConnErr(err) {
+        console.log("Connection error: " + err);
+    }
+}
+
+
 
 /*
 var server = net.createServer(function(socket) {
