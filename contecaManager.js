f3f2d83568d3077157627a8be80d55ce713d6c5c
Stefano Ruschetta <stefano@MacBook-Pro-di-Stefano.local>
2018-02-22 13:39:11 +0100

    private module

---
+++ contecaManager.js
@@ -0,0 +1,78 @@
+const net = require('net');
+const mbus = require('./mbus');
+
+var server = null;
+
+
+module.exports = {
+
+    listeningPort : null,
+
+    createServer : function(listeningPort) {
+
+        this.listeningPort = listeningPort;
+
+        server = net.createServer();
+        server.on('connection', handleConnection);
+
+        server.listen (this.listeningPort, function(){
+            console.log("server listening to: %j", server.address());
+        });
+    }
+};
+
+function handleConnection(conn){
+
+    //Connection configuration
+
+    var remoteAddress = conn.remoteAddress + ":" + conn.remotePort;
+
+    conn.setEncoding("hex");
+
+    conn.on("data", onConnData);
+    conn.on("close", onConnClose);
+    conn.on("err", onConnErr);
+
+    function onConnData(data) {
+
+        if (mbus.MBusStatus === mbus.MBusStatusEnum.waitingForAck && data === "e5") {
+
+            console.log("Ack received for: " + mbus.currentPrimaryAddress);
+            console.log("Sending data request to: " + mbus.currentPrimaryAddress);
+
+            mbus.MBusStatus = mbus.MBusStatusEnum.waitingForData;
+
+            conn.write(mbus.dataForPrimaryAddress(mbus.currentPrimaryAddress), "hex");
+        }
+        else if (mbus.MBusStatus === mbus.MBusStatusEnum.waitingForData) {
+            console.log("Data received for: " + mbus.currentPrimaryAddress);
+            console.log("Data: " + data);
+
+            if (mbus.checkResponseValidity(data)) {
+
+            }
+            else {
+                console.log("invalid response");
+            }
+
+            conn.destroy();
+        }
+    }
+
+    function onConnClose(){
+        console.log("Connection close");
+    }
+
+    function onConnErr(err) {
+        console.log("Connection error: " + err);
+    }
+
+
+    //Data
+    console.log("New client connection from: " + remoteAddress);
+
+    mbus.currentPrimaryAddress = "01";
+    mbus.MBusStatus = mbus.MBusStatusEnum.waitingForAck;
+
+    conn.write( mbus.ackForPrimaryAddress(mbus.currentPrimaryAddress), "hex");
+}
