d476503a802761387e76a1dcef3bd7201e6bc892
Stefano Ruschetta <stefano@MacBook-Pro-di-Stefano.local>
2018-02-23 16:06:19 +0100

    DB

--- contecaDB.js
+++ contecaDB.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
 
 const DBHost = 'mongodb://localhost/contecanetwork';
 
@@ -14,6 +15,8 @@ function connectToDB() {
     db.on('error', console.error.bind(console, 'connection error:'));
     db.once('open', function() {
         console.log("connection to DB successfull");
+
+        createDummyDB();
     });
 }
 
@@ -31,16 +34,28 @@ var measureSchema = mongoose.Schema({
     volume_2: String,
     volume_3: String,
     volume_4: String,
-    energy_1: String
+    energy_1: String,
+    related_conteca: { type: Schema.Types.ObjectId, ref: 'Conteca' }
 });
 
+var contecaSchema = mongoose.Schema({
+    primary_address: String,
+    related_concentrator: { type: Schema.Types.ObjectId, ref: 'Concentrator' }
+});
+
+var concentratorSchema = mongoose.Schema({
+    name: String
+});
 
 var Measure = mongoose.model("Measure", measureSchema);
+var Conteca = mongoose.model("Conteca", contecaSchema);
+var Concentrator = mongoose.model("Concentrator", concentratorSchema);
+
 
 //TODO: Create objects
 
 function createMeasure(energy, volume, power, volume_flow, flow_temperature, return_temperature,
-                       volume_1, volume_2, volume_3, volume_4, energy_1){
+                       volume_1, volume_2, volume_3, volume_4, energy_1, related_conteca){
 
     var measure = new Measure({ timestamp: Date.now(),
                                 energy: energy,
@@ -53,7 +68,8 @@ function createMeasure(energy, volume, power, volume_flow, flow_temperature, ret
                                 volume_2: volume_2,
                                 volume_3: volume_3,
                                 volume_4: volume_4,
-                                energy_1: energy_1 });
+                                energy_1: energy_1,
+                                related_conteca: related_conteca});
 
     measure.save(function(err, measure){
         if(err) {
@@ -63,6 +79,48 @@ function createMeasure(energy, volume, power, volume_flow, flow_temperature, ret
             console.log("Measure: " + measure + "correctly saved on DB.");
         }
     });
+}
+
+
+function createConcentrator(name) {
+    var concentrator = new Concentrator(name);
+
+    concentrator.save(function(err, concentrator) {
+        if (err) {
+            console.log("error: " + err + "saving" + concentrator);
+        }
+        else {
+            console.log("Concentrator: " + concentrator + "correctly saved on DB.");
+        }
+    });
+}
+
+function createConteca(primary_address, concentrator) {
+    var conteca = new Conteca(primary_address, concentrator);
+
+    conteca.save(function(err, conteca) {
+        if (err) {
+            console.log("error: " + err + "saving" + conteca);
+        }
+        else {
+            console.log("Conteca: " + conteca + "correctly saved on DB.");
+        }
+    });
+}
+
+
+function createDummyDB(){
+    //Find concentrators
+    Concentrator.findOne(function(err, result){
+        if (!err) {
+            console.log("Found: " + result);
+            
+
+        }
+    });
+
+    //if not concentrator
+    //create dummy concentrator wit two conteca
 }
 
 
