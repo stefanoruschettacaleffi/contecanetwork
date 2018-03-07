cbf8796ff2d8fd1282a90d54f409e9915b4fa072
Stefano Ruschetta <stefano@MacBook-Pro-di-Stefano.local>
2018-03-07 14:49:02 +0100

    DB

--- contecaDB.js
+++ contecaDB.js
@@ -127,7 +127,7 @@ function createConteca(primary_address, related_concentrator, callback) {
 /*--- Read ---*/
 
 function getAllConcentrators(callback) {
-  
+
     Concentrator.find({}, 'name serial address city', function (err, result) {
         callback(err, result);
     })
